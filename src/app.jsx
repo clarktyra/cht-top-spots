f5b9cfab51b97d0e169de0efcc248872c6b39b35
clarktyra <clark.tyra@gmail.com>
2018-08-14 17:26:53 -0700

    Changed the backgrounds and installed a header animation

--- src/app.jsx
+++ src/app.jsx
@@ -6,52 +6,41 @@ import TopSpot from './topspot';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      topspots:[] 
-
+      topspots: []
     }
   }
-  
   componentWillMount() {
     axios
-    .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
-    .then(response => response.data)
-    .then(topspots => this.setState({ topspots }));
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      .then(response => response.data)
+      .then(topspots => this.setState({ topspots }));
   }
-  
-  
-  
-  
   render() {
-    
     return (
-      
-      <div className='App someshit'>
-      
-      <div className="container somethin" style={{border: "solid"}} >
-        <h1>San Diego Top Spots</h1>
-        <h4>A list of the top 30 places to see in San Diego</h4>
-      </div>
-     
-      <div className="container somefat" style={{border: "dotted"}}>
-      { 
-    this.state.topspots.map(topspot => (
-        <TopSpot
-            key={topspot.id}
-            name={topspot.name}
-            description={topspot.description}
-            location={topspot.location} />
-    ))
-}
-      </div> 
+      <div className='App everything'>
+        <div className="container somethin"  >
+          <h1 className="title" >San Diego Top Spots</h1>
+          <h4 className="title">A list of the top 30 places to see in San Diego</h4>
+        </div>
+        <div className="container somefat" >
+          {
+            this.state.topspots.map(topspot => (
+              <TopSpot
+                key={topspot.id}
+                name={topspot.name}
+                description={topspot.description}
+                location={topspot.location} />
+            ))
+          }
+        </div>
 
       </div>
 
-      
+
     );
   }
-  
+
 }
 
 export default App;
